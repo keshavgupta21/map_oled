a8fc2549fa377f72627b00f1b0acfe01d4a4f75b
Keshav Gupta <keshav21@mit.edu>
2021-09-20 19:50:33 -0700

    update references to OSMnx API

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
+from networkx.classes.multidigraph import MultiDiGraph
 import osmnx as ox
 import pickle
 from PIL import Image
 
-# FILENAME = "map.osm"
-# graph = ox.graph_from_file(FILENAME)
+image_bbox = (42.4344, 42.2713, -70.8802, -71.2596) # boston
+name = "boston"
+# image_bbox = (21.2890, 21.0834, 81.1655, 81.5449) # bhilai
+# name = "bhilai"
+# graph = ox.graph_from_bbox(*image_bbox, truncate_by_edge=True)
 
-# with open("graphs.pkl", "wb") as f:
-    # pickle.dump(graph, f)
+# with open("graphs_{0}.pkl".format(name), "wb") as f:
+#     pickle.dump(graph, f)
 
-with open("graphs.pkl", "rb") as f:
+with open("graphs_{0}.pkl".format(name), "rb") as f:
    ref_graph = pickle.load(f)
 
 road_types = {
-    'trunk': 1,
-    'tertiary': 0.4,
-    'secondary': 0.5,
-    'primary': 0.75,
-    'residential': 0.075,
-    'track': 0.1,
+    'trunk': 0.5,
+    'tertiary': 0.2,
+    'secondary': 0.25,
+    'primary': 0.375,
+    'residential': 0.1,
+    'track': 0,
     'unclassified': 0,
     'service': 0,
     'footway': 0,
-    'secondary_link': 0,
-    'tertiary_link': 0
+    'primary_link': 0.5,
+    'secondary_link': 0.25,
+    'tertiary_link': 0.2,
+    'motorway': 1,
 }
-cumulative_image = Image.new("RGBA", (1674, 2547))
+cumulative_image = None
 
 for cur_rt in road_types:
-    with open("graphs.pkl", "rb") as f:
-        wrk_graph = pickle.load(f)
-    for (u, v, k, d) in ref_graph.edges(keys = True, data = True):
-        hw = d.get("highway", None)
-        if type(hw) is not list and hw != cur_rt:
-            wrk_graph.remove_edge(u, v, k)
-    ox.plot_graph(wrk_graph, node_size = 0, 
-        bbox = (21.4092, 20.9591, 81.5346, 81.2176), 
-        margin = 0, show = False, save = True, 
-        filename = cur_rt, dpi = 240, 
-        fig_height = 16, fig_width = 9, 
-        bgcolor = "#000000", edge_color = "#ffffff",
-        edge_linewidth = road_types[cur_rt])
-    img = Image.open("images/" + cur_rt + ".png")
-    cumulative_image = Image.blend(cumulative_image, img, 0.5)
-    cumulative_image = Image.eval(cumulative_image, lambda x: x*2)
+    if road_types[cur_rt] > 0:
+        with open("graphs_{0}.pkl".format(name), "rb") as f:
+            wrk_graph = pickle.load(f)
+        for (u, v, k, d) in ref_graph.edges(keys = True, data = True):
+            hw = d.get("highway", None)
+            if type(hw) is not list and hw != cur_rt:
+                if wrk_graph.has_edge(u, v, k):
+                    wrk_graph.remove_edge(u, v, k)
+        ox.plot_graph(wrk_graph, node_size = 0, 
+            bbox = image_bbox,
+            show = False, save = True, 
+            filepath = "images/" + cur_rt + ".png", dpi = 600,
+            bgcolor = "#000000", edge_color = "#ffffff",
+            edge_linewidth = road_types[cur_rt])
+        img = Image.open("images/" + cur_rt + ".png")
+        if cumulative_image is None:
+            cumulative_image = Image.new("RGBA", img.size)
+        
+        cumulative_image = Image.blend(cumulative_image, img, 0.5)
+        cumulative_image = Image.eval(cumulative_image, lambda x: x*2)
 cumulative_image.save("final.png")
     
